9d83fd9bdc0b92038e75de1035324ee763a1ef34
Harryburn <68109900+Harryburn@users.noreply.github.com>
2020-09-03 08:10:14 +0100

    Auto Commit

---
+++ cypress/integration/Test_TestsGoToUrlAssertURLNegativeEnterFirstNameClickCreateanAccount4.js
@@ -0,0 +1,27 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    debugger
+    return false
+})
+
+Cypress.on('fail', (err) => {
+    debugger
+})
+
+
+
+describe("{{{ModelName}}}", function() {
+   it("TestsGoToUrlAssertURLNegativeEnterFirstNameClickCreateanAccount4", function() {
+ 	
+ 	cy.visit('https://magento.nublue.co.uk/customer/account/create/')
+
+cy.url().should('eq', 'https://magento.nublue.co.uk/customer/account/create/')
+
+cy.xpath("//*[@id='firstname']").type("389348")
+
+cy.get("button[class='action\\ submit\\ primary']").click()
+
+   })
+})
+
