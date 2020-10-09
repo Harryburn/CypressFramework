7af88103da9ce4b20bdc35e1e71a69d1ffa565e5
Harryburn <68109900+Harryburn@users.noreply.github.com>
2020-10-09 13:05:39 +0100

    Auto Commit

---
+++ cypress/integration/Example_GoToUrlAssertURL.js
@@ -0,0 +1,23 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    debugger
+    return false
+})
+
+Cypress.on('fail', (err) => {
+    debugger
+})
+
+
+
+describe("GoToUrlAssertURL", function() {
+   it("GoToUrlAssertURL", function() {
+ 	
+ 	cy.visit('https://magento.nublue.co.uk/customer/account/create/')
+
+cy.url().should('eq', 'https://magento.nublue.co.uk/customer/account/create/')
+
+   })
+})
+
